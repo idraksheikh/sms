cfe9a1af0ef37476808703edcf6d56b73cbd3f93
Gurpreet Singh <gurpreetachint@gmail.com>
2023-04-23 16:54:57 +0530

    upload_fees

--- lib/Screens/styles/font.dart
+++ lib/Screens/styles/font.dart
@@ -4,16 +4,21 @@ import 'package:google_fonts/google_fonts.dart';
 class ThemeFontStyle {
   final double fontSize;
   final Color color;
+  final double height;
+
   ThemeFontStyle({
     Key? key,
     required this.fontSize,
     this.color = const Color.fromRGBO(101, 103, 107, 1),
+    this.height = 1,
   });
+
   late TextStyle style = GoogleFonts.lato(
       textStyle: TextStyle(
     fontSize: fontSize,
     fontWeight: FontWeight.bold,
     fontFamily: 'Oswald',
     color: color,
+    height: height,
   ));
 }
