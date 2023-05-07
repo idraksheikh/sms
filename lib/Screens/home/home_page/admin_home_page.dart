2e2619bca4ab5537c2bd74f41fe9c8df0d369818
Idrak Sheikh <idraksheikh333@gmail.com>
2023-05-08 01:12:44 +0530

    Added Admin home Page.

--- lib/Screens/home/home_page/admin_home_page.dart
+++ lib/Screens/home/home_page/admin_home_page.dart
@@ -1,5 +1,10 @@
+import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/container.dart';
 import 'package:flutter/src/widgets/framework.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+import '../../widgets/spacer.dart';
 
 class AdminHomePage extends StatefulWidget {
   const AdminHomePage({super.key});
@@ -11,6 +16,58 @@ class AdminHomePage extends StatefulWidget {
 class _AdminHomePageState extends State<AdminHomePage> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Container(
+      child: Padding(
+        padding: const EdgeInsets.only(top:30,left: 15, right: 15),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
+              Text(
+                'Welcome, Gurpreet',
+                style: GoogleFonts.lato(
+                  textStyle: const TextStyle(
+                    fontSize: 16,
+                    fontWeight: FontWeight.bold,
+                    color: Color.fromRGBO(101, 103, 107, 1),
+                  ),
+                ),
+              ),
+              Text(
+                'Your Details',
+                style: GoogleFonts.lato(
+                  textStyle: const TextStyle(
+                    fontSize: 20,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ),
+              const Space(height: 10),
+            ]),
+            const Space(
+              width: 25,
+            ),
+            Container(
+              alignment: Alignment.topRight,
+              height: 20,
+              // width: 20,
+              // margin: const EdgeInsets.only(right: 20),
+              child: InkWell(
+                onTap: (() async {
+                  SharedPreferences preference =
+                      await SharedPreferences.getInstance();
+                  preference.setString('registration_id', '');
+                  preference.setString('password', '');
+                  Navigator.pushNamed(context, '/wrapper');
+                }),
+                child: const Image(
+                  image: AssetImage('assets/images/logout.png'),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
